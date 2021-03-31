f1cbab44fe0595b0155f8c604cd14b01f8b33d54
semigris <semigris@gmail.com>
2021-03-31 13:44:23 +0300

    modified '0'

--- include/mine.h
+++ include/mine.h
@@ -1,9 +1,11 @@
-//
-// Created by kstout on 3/26/21.
-//
-
 #ifndef MINISHELL_D_H
 #define MINISHELL_D_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/shell.h"
+#include "../include/get_next_line.h"
+#include <wait.h>
 
 typedef struct s_shell
 {
@@ -12,14 +14,23 @@ typedef struct s_shell
 	int lenght;
 	int dollen;
 	int numargs;
-	char dkey[15];
-	char *dvalue;
+	char *evkey;
+	char *evvalue;
+	int valuelen;
 	char **args_of_shell;
 }				t_shell;
 
 int parse_shell(t_shell *sh, char *line, int i);
 void if_find_elem(char *arg, int *i, t_shell *sh);
-int write_args(t_shell *sh, char *arg, int *i, char till);
-void init_aos(t_shell *sh);
+int prepare_array(t_shell *sh, char *arg, int *i, char till);
+void init_shell_struct(t_shell *sh);
+int init_env_var(char *dollar, t_shell *sh);
+int find_lenght(char *line, char till, int i);
+int find_value(t_shell *sh);
+char *init_array(t_shell *sh, int linelen);
+int create_words(t_shell *sh, char *line, int *i);
+int correct_env_var(char c);
+int create_tokens (t_shell *sh, char *line, int *i, int linelen);
+//int create_env(t_shell *sh, int *z, int *k, int linelen);
 
 #endif //MINISHELL_D_H
